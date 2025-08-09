8358c21304d9c5b72073031513ef42d60e4de952
Braden Everson <bradeneverson@gmail.com>
2025-08-09 16:27:05 -0500

    Create a setting method, we need something better than comptime int tho

--- bitmask.zig
+++ bitmask.zig
@@ -2,12 +2,17 @@ const std = @import("std");
 
 pub fn Bitmask(comptime size: u16) type {
     return packed struct {
-        bits: std.meta.Int(.unsigned, size),
+        bits: IntType,
 
         const Self = @This();
+        const IntType = std.meta.Int(.unsigned, size);
 
         pub fn init() Self {
             return Self{ .bits = 0 };
+        }
+
+        pub fn set(self: *Self, idx: comptime_int) void {
+            self.bits |= @as(IntType, 1) << idx;
         }
     };
 }
@@ -15,4 +20,15 @@ pub fn Bitmask(comptime size: u16) type {
 test "packed bitmask size" {
     const mask_type = Bitmask(1024);
     try std.testing.expectEqual(1024, @bitSizeOf(mask_type));
+}
+
+test "initializes to 0" {
+    const mask = Bitmask(16).init();
+    try std.testing.expectEqual(0, mask.bits);
+}
+
+test "setting a bit" {
+    var mask = Bitmask(16).init();
+    mask.set(0);
+    try std.testing.expectEqual(1, mask.bits);
 }
